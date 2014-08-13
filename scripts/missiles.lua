206c30c6e27f14a39d0bedd60da972f40314afaa
Andrettin <andre.ng@live.com>
2014-08-13 09:06:25 +0200

    Added new war machine graphics, and flaming rock and tossed goblin missiles

--- scripts/missiles.lua
+++ scripts/missiles.lua
@@ -44,6 +44,18 @@ DefineMissileType("missile-dwarven-ballista-bolt",
   Class = "missile-class-point-to-point", Sleep = 1, Speed = 8, Range = 2,
   ImpactMissile = "missile-impact", SplashFactor = 4 } )
 
+DefineMissileType("missile-catapult-flaming-rock",
+  { File = "missiles/catapult_flaming_rock.png", Size = {32, 32}, Frames = 15, NumDirections = 9,
+  ImpactSound = "explosion",
+  DrawLevel = 50, Class = "missile-class-point-to-point-cycle-once", Sleep = 1, Speed = 8, Range = 2,
+  ImpactMissile = "missile-impact", SplashFactor = 4 } )
+  
+DefineMissileType("missile-throwing-goblin",
+   { File = "missiles/gobtoss.png", Size = {32, 32}, Frames = 40, NumDirections = 9,
+   ImpactSound = "explosion",
+   DrawLevel = 50, Class = "missile-class-point-to-point-cycle-once", Sleep = 1, Speed = 6, Range = 2,
+   ImpactMissile = "missile-impact", SplashFactor = 4  } )
+
 DefineMissileType("missile-impact",
   { File = "missiles/siege_projectile_impact.png", Size = {48, 48}, Frames = 10, NumDirections = 1,
   DrawLevel = 50, Class = "missile-class-stay", Sleep = 2, Speed = 16, Range = 1 } )
