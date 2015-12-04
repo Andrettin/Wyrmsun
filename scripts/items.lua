7b7ae1afdcbf701545e2423acc30081e813bd48c
Andrettin <andre.ng@live.com>
2015-12-05 00:45:05 +0100

    Added some item definitions

---
+++ scripts/items.lua
@@ -0,0 +1,96 @@
+--       _________ __                 __
+--      /   _____//  |_____________ _/  |______     ____  __ __  ______
+--      \_____  \\   __\_  __ \__  \\   __\__  \   / ___\|  |  \/  ___/
+--      /        \|  |  |  | \// __ \|  |  / __ \_/ /_/  >  |  /\___ \
+--     /_______  /|__|  |__|  (____  /__| (____  /\___  /|____//____  >
+--             \/                  \/          \//_____/            \/
+--  ______________________                           ______________________
+--                        T H E   W A R   B E G I N S
+--         Stratagus - A free fantasy real time strategy game engine
+--
+--      (c) Copyright 2015 by Andrettin
+--
+--      This program is free software; you can redistribute it and/or modify
+--      it under the terms of the GNU General Public License as published by
+--      the Free Software Foundation; either version 2 of the License, or
+--      (at your option) any later version.
+--
+--      This program is distributed in the hope that it will be useful,
+--      but WITHOUT ANY WARRANTY; without even the implied warranty of
+--      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--      GNU General Public License for more details.
+--
+--      You should have received a copy of the GNU General Public License
+--      along with this program; if not, write to the Free Software
+--      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+
+DefineItemType("Short Sword", {
+	Class = "Sword",
+	Icon = "icon-germanic-short-sword"
+})
+
+DefineItemType("Broad Sword", {
+	Class = "Sword",
+	Icon = "icon-germanic-broad-sword"
+})
+
+DefineItemType("Spatha", {
+	Class = "Sword",
+	Icon = "icon-teuton-spatha"
+})
+
+DefineItemType("Battle Axe", {
+	Class = "Axe",
+	Icon = "icon-dwarven-axe-1"
+})
+
+DefineItemType("Broad Axe", {
+	Class = "Axe",
+	Icon = "icon-dwarven-axe-2"
+})
+
+DefineItemType("Great Axe", {
+	Class = "Axe",
+	Icon = "icon-dwarven-axe-3"
+})
+
+DefineItemType("Wooden Shield", {
+	Class = "Shield",
+	Icon = "icon-germanic-wooden-oblong-shield"
+})
+
+DefineItemType("Bronze Shield", {
+	Class = "Shield",
+	Icon = "icon-germanic-bronze-shield"
+})
+
+DefineItemType("Iron Shield", {
+	Class = "Shield",
+	Icon = "icon-teuton-iron-shield"
+})
+
+DefineItemType("Wooden Shield", {
+	Class = "Shield",
+	Icon = "icon-germanic-wooden-oblong-shield"
+})
+
+DefineItemType("Bronze Shield", {
+	Class = "Shield",
+	Icon = "icon-germanic-bronze-shield"
+})
+
+DefineItemType("Round Shield", {
+	Class = "Shield",
+	Icon = "icon-dwarven-shield-1"
+})
+
+DefineItemType("Heater Shield", {
+	Class = "Shield",
+	Icon = "icon-dwarven-shield-2"
+})
+
+DefineItemType("Thrymgjol Shield", {
+	Class = "Shield",
+	Icon = "icon-dwarven-shield-3"
+})
