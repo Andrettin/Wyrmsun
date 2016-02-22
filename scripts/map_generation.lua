23ad4bf8d50388cdf5a9e7bed3995a5dc3b74896
Andrettin <andre.ng@live.com>
2016-02-22 18:38:23 +0100

    Update to fairlimbed tree

--- scripts/map_generation.lua
+++ scripts/map_generation.lua
@@ -10,7 +10,7 @@
 --
 --      map_generation.lua - Defines the map generation scripts.
 --
---      (c) Copyright 2013-2015 by Andrettin
+--      (c) Copyright 2013-2016 by Andrettin
 --
 --      This program is free software; you can redistribute it and/or modify
 --      it under the terms of the GNU General Public License as published by
