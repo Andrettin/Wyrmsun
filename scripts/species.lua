b53985d4d136850450c749552fe86ee79dd820a3
Andrettin <andre.ng@live.com>
2016-07-04 23:16:08 +0200

    Added a bit more species data

--- scripts/species.lua
+++ scripts/species.lua
@@ -25,20 +25,8 @@
 --      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
 
-DefineSpecies("adelobasileus", { -- Spencer G. Lucas and Zhexi Luo, "Adelobasileus from the Upper Triassic of West Texas: The Oldest Mammal", 1993, p. 309.
-	Name = "Adelobasileus",
-	Genus = "adelobasileus",
-	Species = "cromptoni",
-	ChildUpgrade = "upgrade-child"
-})
-
-DefineSpecies("ilingoceros", {
-	Name = "Ilingoceros",
-	ChildUpgrade = "upgrade-child"
-})
-
-DefineSpecies("goat", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=180715
-	Name = "Goat",
+DefineSpeciesFamily("antilocapridae", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=552305
+	Name = "Antilocapridae",
 	Kingdom = "animalia",
 	Subkingdom = "bilateria",
 	Infrakingdom = "deuterostomia",
@@ -49,17 +37,11 @@ DefineSpecies("goat", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleR
 	Class = "mammalia",
 	Subclass = "theria",
 	Infraclass = "eutheria",
-	Order = "artiodactyla",
-	Family = "bovidae",
-	Subfamily = "caprinae",
-	Genus = "capra",
-	Species = "hircus",
-	ChildUpgrade = "upgrade-child",
-	EvolvesFrom = {"ilingoceros"}
+	Order = "artiodactyla"
 })
 
-DefineSpecies("yale", {
-	Name = "Yale",
+DefineSpeciesFamily("bovidae", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=180704
+	Name = "Bovidae",
 	Kingdom = "animalia",
 	Subkingdom = "bilateria",
 	Infrakingdom = "deuterostomia",
@@ -70,16 +52,11 @@ DefineSpecies("yale", {
 	Class = "mammalia",
 	Subclass = "theria",
 	Infraclass = "eutheria",
-	Order = "artiodactyla",
-	Family = "bovidae",
-	Subfamily = "caprinae",
-	Genus = "eale",
-	ChildUpgrade = "upgrade-child",
-	EvolvesFrom = {"ilingoceros"}
+	Order = "artiodactyla"
 })
 
-DefineSpecies("horse", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=180691
-	Name = "Horse",
+DefineSpeciesFamily("equidae", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=180688
+	Name = "Equidae",
 	Kingdom = "animalia",
 	Subkingdom = "bilateria",
 	Infrakingdom = "deuterostomia",
@@ -90,15 +67,29 @@ DefineSpecies("horse", { -- Source: http://www.itis.gov/servlet/SingleRpt/Single
 	Class = "mammalia",
 	Subclass = "theria",
 	Infraclass = "eutheria",
-	Order = "perissodactyla",
-	Family = "equidae",
-	Genus = "equus",
-	Species = "caballus",
-	ChildUpgrade = "upgrade-child"
+	Order = "perissodactyla"
 })
 
-DefineSpecies("rat", { -- http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=180363
-	Name = "Rat",
+DefineSpeciesFamily("hominidae", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=180090
+	Name = "Hominidae",
+	Kingdom = "animalia",
+	Subkingdom = "bilateria",
+	Infrakingdom = "deuterostomia",
+	Phylum = "chordata",
+	Subphylum = "vertebrata",
+	Infraphylum = "gnathostomata",
+	Superclass = "tetrapoda",
+	Class = "mammalia",
+	Subclass = "theria",
+	Infraclass = "eutheria",
+	Order = "primates",
+	Suborder = "haplorrhini",
+	Infraorder = "simiiformes",
+	Superfamily = "hominoidea"
+})
+
+DefineSpeciesFamily("muridae", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=180360
+	Name = "Muridae",
 	Kingdom = "animalia",
 	Subkingdom = "bilateria",
 	Infrakingdom = "deuterostomia",
@@ -111,16 +102,11 @@ DefineSpecies("rat", { -- http://www.itis.gov/servlet/SingleRpt/SingleRpt?search
 	Infraclass = "eutheria",
 	Order = "rodentia",
 	Suborder = "myomorpha",
-	Superfamily = "muroidea",
-	Family = "muridae",
-	Subfamily = "murinae",
-	Genus = "rattus",
-	Species = "norvegicus",
-	ChildUpgrade = "upgrade-child"
+	Superfamily = "muroidea"
 })
 
-DefineSpecies("squirrel", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=632439
-	Name = "Squirrel", -- Eurasian Red Squirrel
+DefineSpeciesFamily("sciuridae", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=180135
+	Name = "Sciuridae",
 	Kingdom = "animalia",
 	Subkingdom = "bilateria",
 	Infrakingdom = "deuterostomia",
@@ -132,13 +118,177 @@ DefineSpecies("squirrel", { -- Source: http://www.itis.gov/servlet/SingleRpt/Sin
 	Subclass = "theria",
 	Infraclass = "eutheria",
 	Order = "rodentia",
-	Suborder = "sciuromorpha",
+	Suborder = "sciuromorpha"
+})
+
+DefineSpecies("adelobasileus", { -- Source: Spencer G. Lucas and Zhexi Luo, "Adelobasileus from the Upper Triassic of West Texas: The Oldest Mammal", 1993, p. 309.
+	Name = "Adelobasileus",
+	Genus = "adelobasileus",
+	Species = "cromptoni",
+	ChildUpgrade = "upgrade-child"
+})
+
+DefineSpecies("ilingoceros", { -- Source: http://fossilworks.org/bridge.pl?a=taxonInfo&taxon_no=42728
+	Name = "Ilingoceros",
+	Family = "antilocapridae",
+	Genus = "ilingoceros",
+	Species = "alexandrae", -- type species of the Ilingoceros genus
+	ChildUpgrade = "upgrade-child",
+	EvolvesFrom = {"adelobasileus"} -- possibly, since Adelobasileus is a very early mammal (or is related to the earliest mammals)
+	-- ground dwelling browser-grazer
+})
+
+DefineSpecies("aragoral", { -- Source: http://fossilworks.org/bridge.pl?action=taxonInfo&taxon_no=86341
+	Name = "Aragoral",
+	Family = "bovidae",
+	Subfamily = "caprinae",
+	Genus = "aragoral",
+	Species = "mudejar", -- sole species of the Aragoral genus
+	ChildUpgrade = "upgrade-child"
+	-- ground dwelling grazer-browser
+})
+
+DefineSpecies("goat", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=180715
+	Name = "Goat",
+	Family = "bovidae",
+	Subfamily = "caprinae",
+	Genus = "capra",
+	Species = "hircus",
+	ChildUpgrade = "upgrade-child",
+	EvolvesFrom = {"aragoral"} -- oldest member of the Caprinae subfamily
+})
+
+DefineSpecies("yale", {
+	Name = "Yale",
+	Family = "bovidae",
+	Subfamily = "caprinae",
+	Genus = "eale",
+	ChildUpgrade = "upgrade-child",
+	EvolvesFrom = {"aragoral"} -- oldest member of the Caprinae subfamily
+})
+
+DefineSpecies("horse", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=180691
+	Name = "Horse",
+	Family = "equidae",
+	Genus = "equus",
+	Species = "caballus",
+	ChildUpgrade = "upgrade-child"
+})
+
+DefineSpecies("rat", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=180363
+	Name = "Rat",
+	Family = "muridae",
+	Subfamily = "murinae",
+	Genus = "rattus",
+	Species = "norvegicus",
+	ChildUpgrade = "upgrade-child"
+})
+
+DefineSpecies("squirrel", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=632439
+	Name = "Squirrel", -- Eurasian Red Squirrel
 	Family = "sciuridae",
 	Subfamily = "sciurinae",
 	Tribe = "sciurini",
 	Genus = "sciurus",
 	Species = "vulgaris",
 	ChildUpgrade = "upgrade-child"
+})
+
+DefineSpecies("proailurus", { -- Source: http://fossilworks.org/bridge.pl?action=taxonInfo&taxon_no=40900#
+	Name = "Proailurus",
+	Genus = "proailurus",
+	Species = "lemanensis", -- sole species of the Proailurus genus
+	ChildUpgrade = "upgrade-child",
+	EvolvesFrom = {"adelobasileus"} -- possibly, since Adelobasileus is a very early mammal (or is related to the earliest mammals)
+	-- ground dwelling carnivore-omnivore
+	-- likely ancestor of sabertooths and modern cats; Source: http://www.wildworldvisuals.com/product/proailurus/
+})
+
+DefineSpecies("australopithecus", {
+	Name = "Australopithecus",
+	Family = "hominidae", -- Source: http://fossilworks.org/bridge.pl?action=taxonInfo&taxon_no=40900 ; Source: http://fossilworks.org/bridge.pl?a=taxonInfo&taxon_no=83061
+	Genus = "australopithecus",
+	Species = "afarensis",
+	ChildUpgrade = "upgrade-child"
+})
+
+DefineSpecies("dwarf", {
+	Name = "Dwarf",
+	Family = "hominidae",
+	Genus = "nanus", -- "dwarf" in Latin
+	Species = "sapiens",
+	EvolvesFrom = {"australopithecus"}, -- could descend from another closely-related species instead; dwarves and gnomes should have a common ancestor
+	Sapient = true
+})
+
+DefineSpecies("elf", {
+	Name = "Elf",
+	Family = "hominidae",
+	Genus = "alfus", -- "elf" in Latin
+	Species = "sapiens",
+	EvolvesFrom = {"australopithecus"}, -- could descend from another closely-related species instead
+	Sapient = true
+})
+
+DefineSpecies("gnome", {
+	Name = "Gnome",
+	Family = "hominidae",
+	Genus = "gnomus", -- "gnome" in Latin
+	Species = "sapiens",
+	EvolvesFrom = {"australopithecus"}, -- could descend from another closely-related species instead; dwarves and gnomes should have a common ancestor
+	Sapient = true
+	-- in prehistoric times, the ancestors of dwarves and gnomes were transported to Nidavellir by the magic of the Aesir
+})
+
+DefineSpecies("derro", {
+	Name = "Derro",
+	Family = "hominidae",
+	Genus = "gnomus", -- "gnome" in Latin
+	Species = "detrimenti", -- "detrimental" in Latin
+	EvolvesFrom = {"australopithecus"}, -- could descend from another closely-related species instead; dwarves and gnomes should have a common ancestor
+	Sapient = true
+	-- in prehistoric times, the ancestors of dwarves and gnomes were transported to Nidavellir by the magic of the Aesir
+})
+
+DefineSpecies("human", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=180092
+	Name = "Human",
+	Family = "hominidae",
+	Subfamily = "homininae",
+	Genus = "homo",
+	Species = "sapiens",
+	EvolvesFrom = {"australopithecus"}, -- possibly
+	Sapient = true
+})
+
+DefineSpecies("ettin", {
+	Name = "Ettin",
+	Genus = "gigans", -- "giant" in Latin
+	Species = "sapiens",
+	Sapient = true
+})
+
+DefineSpecies("goblin", {
+	Name = "Goblin",
+	Genus = "gobelinus", -- "goblin" in Latin
+	Species = "sapiens",
+	Sapient = true
+})
+
+DefineSpecies("kobold", { -- reptilian
+	Name = "Kobold",
+	Sapient = true
+})
+
+DefineSpecies("orc", {
+	Name = "Orc",
+	Genus = "orcus", -- "orc" in Latin
+	Species = "sapiens",
+	Sapient = true
+})
+
+DefineSpecies("troll", {
+	Name = "Troll",
+	Sapient = true
 })
 
 DefineSpecies("bat", {
