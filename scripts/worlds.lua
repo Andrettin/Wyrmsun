88ab016b1ddf7f34ba07549e330626370588e375
Andrettin <andre.ng@live.com>
2016-07-05 09:24:49 +0200

    Added base terrain field for worlds

--- scripts/worlds.lua
+++ scripts/worlds.lua
@@ -28,12 +28,14 @@
 DefineWorld("Alfheim", {
 	Plane = "Material Plane",
 	Description = _("The lush world of Alfheim is inhabited by elves and trolls. Its forests are characterized by the beautiful golden foliage of the fairlimbed trees, which are much adored by the elves. The planet orbits a star known as Fairwheel, and is in turn orbited by a moon called Timeteller. Centuries after the elves first achieved spacefaring capability, they decided to use their advanced technology to create an artificial \"sun\" for Alfheim - in truth a fiery moon - to lengthen the days and shorten the darkness. Eventually, the elves wanted to live under sunlight at all times, and the construction of a third \"sun\" started. An accident during the construction caused the third-sun-in-the-making to blow up, causing devastation on the planet. This catastrophe, added with the unnatural existence of the second sun, caused the earth to be scorched with high temperatures, and thus the fields dried up, forests died, and swamps turned into mud-cracked plains."),
-	Background = _("Alfheim was the world of the light elves in Norse mythology. In the ~<Alvissmol~> it is mentioned that elves' term for \"wood\" is \"fair-limbed\". In that same poem, \"the Teller of Time\" is said to be the elves' term for the moon, and \"Fair Wheel\" their term for the sun. The ~<Grimnismol~> mentions that Alfheim was given to the Norse god Freyr by the other deities.")
+	Background = _("Alfheim was the world of the light elves in Norse mythology. In the ~<Alvissmol~> it is mentioned that elves' term for \"wood\" is \"fair-limbed\". In that same poem, \"the Teller of Time\" is said to be the elves' term for the moon, and \"Fair Wheel\" their term for the sun. The ~<Grimnismol~> mentions that Alfheim was given to the Norse god Freyr by the other deities."),
+	BaseTerrain = "Plains"
 })
 
 DefineWorld("Earth", {
 	Plane = "Material Plane",
-	Description = _("The home planet of human beings, a myriad of generations have been born and died on Earth. History has witnessed the rise and fall of innumerous great powers, with few being able to stand the test of time. The first human civilization was Sumeria, and later times saw the appearance of the ancient Greeks and Romans. The mapping of the world progressed slowly, but steadily. The advancement of naval techniques in the 15th and 16h centuries allowed the world's oceans to be explored, and by the late 19th century the inner part of the continents had been fully mapped. The 20th century saw the beginnings of space exploration, with a man setting foot on the moon for the first time. By the 22nd century spacefaring had become common.")
+	Description = _("The home planet of human beings, a myriad of generations have been born and died on Earth. History has witnessed the rise and fall of innumerous great powers, with few being able to stand the test of time. The first human civilization was Sumeria, and later times saw the appearance of the ancient Greeks and Romans. The mapping of the world progressed slowly, but steadily. The advancement of naval techniques in the 15th and 16h centuries allowed the world's oceans to be explored, and by the late 19th century the inner part of the continents had been fully mapped. The 20th century saw the beginnings of space exploration, with a man setting foot on the moon for the first time. By the 22nd century spacefaring had become common."),
+	BaseTerrain = "Plains"
 })
 
 DefineWorld("Jotunheim", {
@@ -51,7 +53,8 @@ DefineWorld("Muspelheim", {
 DefineWorld("Nidavellir", {
 	Plane = "Material Plane",
 	Description = _("The dark plains and mountains of Nidavellir are home to the dwarves, gnomes, goblins and kobolds. Of these diminutive races, the gnomes established the first civilization, and the dwarves the greatest. Nidavellir orbits around a star known as Dvalin's Deceiver, which received its name from Dvalin's dislike of sunlight. The legendary dwarf allegedly preferred the dark even more than most dwarves, complaining incessantly if the sunlight caught him unaware during his travels. The Deep Sea which lies to the east of the dwarven realms of Kal Kartha and Lyr remained unexplored for a long time, until dwarves began building ships robust enough to sail through it. Nidavellir has one moon, which is called \"the Gleamer\". The first contact between dwarves and humans happened in the 23rd century, when an expedition happened upon a dwarven mining operation on a distant planet, resulting in a skirmish."),
-	Background = _("Nidavellir was the world of the dwarves in Norse mythology. The name means \"the Dark Fields\" or \"the Dark Crags\". Nidavellir is mentioned in the ~<Voluspo~>. In the ~<Alvissmol~> it is said that \"the Gleamer\" is the dwarves' term for the moon, while \"the Deceiver of Dvalin\" is their term for the sun.")
+	Background = _("Nidavellir was the world of the dwarves in Norse mythology. The name means \"the Dark Fields\" or \"the Dark Crags\". Nidavellir is mentioned in the ~<Voluspo~>. In the ~<Alvissmol~> it is said that \"the Gleamer\" is the dwarves' term for the moon, while \"the Deceiver of Dvalin\" is their term for the sun."),
+	BaseTerrain = "Dark Plains"
 })
 
 DefineWorld("Niflheim", {
