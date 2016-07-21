aaf09e95c9e230943cf58baeaa1b63a25e40a8a0
Andrettin <andre.ng@live.com>
2016-07-22 01:07:14 +0200

    More felid data

--- scripts/species/mammal/carnivoran/felid.lua
+++ scripts/species/mammal/carnivoran/felid.lua
@@ -31,6 +31,18 @@ DefineSpeciesFamily("felidae", { -- Source: Mauricio Antón and Jorge Morales, "
 	Suborder = "feliformia"
 })
 
+DefineSpeciesGenus("felis", { -- Source: Mauricio Antón and Jorge Morales, "Madrid antes del hombre", 2009, pp. 46-47.
+	Name = "Felis",
+	Family = "felidae",
+	Subfamily = "felinae"
+})
+
+DefineSpeciesGenus("machairodus", { -- Source: Mauricio Antón and Jorge Morales, "Madrid antes del hombre", 2009, pp. 48-49.
+	Name = "Machairodus",
+	Family = "felidae",
+	Subfamily = "machairodontinae"
+})
+
 DefineSpeciesGenus("panthera", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=180592
 	Name = "Panthera",
 	CommonName = "Panther",
@@ -38,21 +50,120 @@ DefineSpeciesGenus("panthera", { -- Source: http://www.itis.gov/servlet/SingleRp
 	Subfamily = "pantherinae"
 })
 
-DefineSpeciesGenus("proailurus", { -- Source: http://fossilworks.org/bridge.pl?action=taxonInfo&taxon_no=41069 ; Source: http://www.marine.csiro.au/mirrorsearch/ir_search.list_species?gen_id=1340242
+DefineSpeciesGenus("pristinosmilus", { -- Source: Mauricio Antón and Jorge Morales, "Madrid antes del hombre", 2009, pp. 48-49.
+	Name = "Pristinosmilus",
+	Family = "felidae",
+	Subfamily = "machairodontinae"
+})
+
+DefineSpeciesGenus("proailurus", { -- Source: Mauricio Antón and Jorge Morales, "Madrid antes del hombre", 2009, p. 46.
 	Name = "Proailurus",
 	Family = "felidae"
 })
 
-DefineSpecies("proailurus", { -- Source: http://fossilworks.org/bridge.pl?action=taxonInfo&taxon_no=41069 ; Source: http://www.marine.csiro.au/mirrorsearch/ir_search.list_species?gen_id=1340242
-	Name = "Proailurus",
+DefineSpeciesGenus("pseudaelurus", { -- Source: Mauricio Antón and Jorge Morales, "Madrid antes del hombre", 2009, pp. 46, 48.
+	Name = "Pseudaelurus",
+	Family = "felidae",
+	Subfamily = "machairodontinae"
+})
+
+DefineSpeciesGenus("schizaelurus", { -- Source: Mauricio Antón and Jorge Morales, "Madrid antes del hombre", 2009, pp. 46-47.
+	Name = "Schizaelurus",
+	Family = "felidae",
+	Subfamily = "felinae"
+})
+
+DefineSpecies("proailurus-lemanensis", { -- Source: Mauricio Antón and Jorge Morales, "Madrid antes del hombre", 2009, p. 46.
+	Name = "Proailurus", -- Proailurus lemanensis
 	Genus = "proailurus",
+	Species = "lemanensis",
 	Homeworld = "Earth",
-	EvolvesFrom = {"nimravus"}, -- possibly, since Nimravus is an earlier feliform
-	Era = "oligocene",
+	EvolvesFrom = {"nimravus"}, -- earlier feliform
+	Era = "oligocene", -- Upper Oligocene
 	Prehistoric = true,
 	ChildUpgrade = "upgrade-child"
-	-- ground dwelling carnivore-omnivore
-	-- likely ancestor of sabertooths and modern cats; Source: http://www.wildworldvisuals.com/product/proailurus/
+	-- first known felid
+	-- had the size of a small lynx
+})
+
+DefineSpecies("felis-attica", { -- Source: Mauricio Antón and Jorge Morales, "Madrid antes del hombre", 2009, pp. 46-47.
+	Name = "Felis", -- Felis attica
+	Genus = "felis",
+	Species = "attica",
+	Homeworld = "Earth",
+	Environments = {"plains"}, -- this species lived in Miocene Madrid, which was mostly arid with a swampy lake in the middle
+	EvolvesFrom = {"proailurus-lemanensis"}, -- earlier felid
+	Era = "miocene", -- Upper Miocene
+	Prehistoric = true,
+	ChildUpgrade = "upgrade-child"
+	-- 35 cm shoulder height
+	-- lived in Eurasia
+	-- as fast and agile as a mountain cat
+})
+
+DefineSpecies("machairodus-aphanistus", { -- Source: Mauricio Antón and Jorge Morales, "Madrid antes del hombre", 2009, pp. 48-49.
+	Name = "Machairodus", -- Machairodus aphanistus
+	Genus = "machairodus",
+	Species = "aphanistus",
+	Homeworld = "Earth",
+	Environments = {"plains"}, -- this species lived in Miocene Madrid, which was mostly arid with a swampy lake in the middle
+	EvolvesFrom = {"proailurus-lemanensis"}, -- earlier felid
+	Era = "miocene", -- Upper Miocene
+	Prehistoric = true,
+	ChildUpgrade = "upgrade-child"
+	-- 100 cm shoulder height
+	-- lived in Eurasia and Africa
+	-- the size of a lion
+	-- his size and strength made him the predominant predator of his environment and era
+	-- high great weight reduced his climbing ability
+})
+
+DefineSpecies("pristinosmilus-ogygia", { -- Source: Mauricio Antón and Jorge Morales, "Madrid antes del hombre", 2009, pp. 48-49.
+	Name = "Pristinosmilus", -- Pristinosmilus ogygia
+	Genus = "pristinosmilus",
+	Species = "ogygia",
+	Homeworld = "Earth",
+	Environments = {"plains"}, -- this species lived in Miocene Madrid, which was mostly arid with a swampy lake in the middle
+	EvolvesFrom = {"proailurus-lemanensis"}, -- earlier felid
+	Era = "miocene", -- Upper Miocene
+	Prehistoric = true,
+	ChildUpgrade = "upgrade-child"
+	-- 60 cm shoulder height
+	-- lived in Eurasia
+	-- had the size similar to that of a modern leopard
+	-- efficient climber
+	-- hunted his prey on land but climbed up trees to escape from the threat of larger predators
+})
+
+DefineSpecies("pseudaelurus-quadridentatus", { -- Source: Mauricio Antón and Jorge Morales, "Madrid antes del hombre", 2009, pp. 46, 48.
+	Name = "Pseudaelurus", -- Pseudaelurus quadridentatus
+	Genus = "pseudaelurus",
+	Species = "quadridentatus",
+	Homeworld = "Earth",
+	EvolvesFrom = {"proailurus-lemanensis"}, -- earlier felid
+	Era = "miocene", -- Middle Miocene
+	Prehistoric = true,
+	ChildUpgrade = "upgrade-child"
+	-- first known machairodontine
+	-- lived in Europe
+	-- somewhat smaller than a puma
+	-- possessed compressed canine teeth
+})
+
+DefineSpecies("schizaelurus-lorteti", { -- Source: Mauricio Antón and Jorge Morales, "Madrid antes del hombre", 2009, pp. 46-47.
+	Name = "Schizaelurus", -- Schizaelurus lorteti
+	Genus = "schizaelurus",
+	Species = "lorteti",
+	Homeworld = "Earth",
+	Environments = {"plains"}, -- this species lived in Miocene Madrid, which was mostly arid with a swampy lake in the middle
+	EvolvesFrom = {"proailurus-lemanensis"}, -- earlier felid
+	Era = "miocene", -- Middle Miocene
+	Prehistoric = true,
+	ChildUpgrade = "upgrade-child"
+	-- had the size of a lynx
+	-- 45 cm shoulder height
+	-- likely inhabited areas with abundant vegetation
+	-- lived in Eurasia
 })
 
 DefineSpecies("lion", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleRpt?search_topic=TSN&search_value=183803
@@ -60,7 +171,7 @@ DefineSpecies("lion", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleR
 	Genus = "panthera",
 	Species = "leo",
 	Homeworld = "Earth",
-	EvolvesFrom = {"proailurus"}, -- possibly, since Proailurus is a much earlier felid
+	EvolvesFrom = {"felis-attica", "schizaelurus-lorteti"}, -- earlier felines (the pantherinae originated from the felinae)
 	Era = "holocene",
 	ChildUpgrade = "upgrade-child"
 })
@@ -68,7 +179,7 @@ DefineSpecies("lion", { -- Source: http://www.itis.gov/servlet/SingleRpt/SingleR
 DefineSpecies("sabertooth", {
 	Name = "Sabertooth",
 	Homeworld = "Earth",
-	EvolvesFrom = {"proailurus"}, -- possibly, since Proailurus is a much earlier felid
+	EvolvesFrom = {"proailurus-lemanensis"}, -- earlier felid
 	Prehistoric = true,
 	ChildUpgrade = "upgrade-child"
 })
@@ -78,7 +189,7 @@ DefineSpecies("tiger", { -- Source: http://www.itis.gov/servlet/SingleRpt/Single
 	Genus = "panthera",
 	Species = "tigris",
 	Homeworld = "Earth",
-	EvolvesFrom = {"proailurus"}, -- possibly, since Proailurus is a much earlier felid
+	EvolvesFrom = {"felis-attica", "schizaelurus-lorteti"}, -- earlier felines (the pantherinae originated from the felinae)
 	Era = "holocene",
 	ChildUpgrade = "upgrade-child"
 })
