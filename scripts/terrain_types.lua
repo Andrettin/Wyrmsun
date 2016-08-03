8339a15193425e7b453008f783a3600d6dd06d9d
Andrettin <andre.ng@live.com>
2016-08-03 21:39:06 +0200

    Added support for mapfield flags in the new terrain type definitions

--- scripts/terrain_types.lua
+++ scripts/terrain_types.lua
@@ -27,50 +27,61 @@
 
 DefineTerrainType("dirt", {
 	Name = "Dirt",
+	Flags = {"land", "no-building", "dirt"},
 	SolidGraphics = {"terrain/dirt/dirt_1.png", "terrain/dirt/dirt_2.png", "terrain/dirt/dirt_3.png"}
 })
 
 DefineTerrainType("mud", {
-	Name = "Mud"
+	Name = "Mud",
+	Flags = {"land", "no-building", "mud"},
+	SolidGraphics = {"terrain/mud/mud_1.png", "terrain/mud/mud_2.png", "terrain/mud/mud_3.png"}
 })
 
 DefineTerrainType("rockbound-cave-floor", {
-	Name = "Rockbound Cave Floor"
+	Name = "Rockbound Cave Floor",
+	Flags = {"land", "no-building", "gravel"}
 })
 
 DefineTerrainType("cave-floor", {
 	Name = "Cave Floor",
 	Buildable = true,
+	Flags = {"land", "stone-floor"},
 	BorderTerrains = {"rockbound-cave-floor"}
 })
 
 DefineTerrainType("dry-mud", {
 	Name = "Dry Mud",
 	Buildable = true,
-	BorderTerrains = {"mud"}
+	Flags = {"land", "mud"},
+	BorderTerrains = {"mud"},
+	SolidGraphics = {"terrain/dry_mud/dry_mud_1.png", "terrain/dry_mud/dry_mud_2.png", "terrain/dry_mud/dry_mud_3.png"}
 })
 
 DefineTerrainType("grass", {
 	Name = "Grass",
 	Buildable = true,
+	Flags = {"land", "grass"},
 	BorderTerrains = {"dirt"},
 	SolidGraphics = {"terrain/grass/grass_1.png", "terrain/grass/grass_2.png", "terrain/grass/grass_3.png"}
 })
 
 DefineTerrainType("shallow-water", {
 	Name = "Shallow Water",
+	Flags = {"water"},
 	BorderTerrains = {"dirt", "mud", "rockbound-cave-floor"}
 })
 
 DefineTerrainType("floor", {
 	Name = "Floor",
 	Buildable = true,
+	Flags = {"land", "stone-floor"},
 	BorderTerrains = {"rockbound-cave-floor"}
 })
 
 DefineTerrainType("rug", {
 	Name = "Rug",
 	Buildable = true,
+	Flags = {"land", "grass"},
 	BorderTerrains = {"floor"}
 })
 
@@ -78,44 +89,51 @@ DefineTerrainType("dungeon-wall", {
 	Name = "Dungeon Wall",
 	Buildable = true,
 	DefaultBaseTerrain = "floor",
-	Overlay = true
+	Overlay = true,
+	Flags = {"land", "wall", "unpassable", "air-unpassable"}
 })
 
 DefineTerrainType("cave-wall", {
 	Name = "Cave Wall",
 	DefaultBaseTerrain = "rockbound-cave-floor",
-	Overlay = true
+	Overlay = true,
+	Flags = {"land", "rock", "unpassable", "air-unpassable"}
 })
 
 DefineTerrainType("rock", {
 	Name = "Rock",
 	DefaultBaseTerrain = "dirt",
-	Overlay = true
+	Overlay = true,
+	Flags = {"land", "rock", "unpassable"}
 })
 
 DefineTerrainType("swamp-rock", {
 	Name = "Rock (Swamp)",
 	DefaultBaseTerrain = "mud",
 	Overlay = true,
+	Flags = {"land", "rock", "unpassable"},
 	SolidGraphics = {"terrain/rock/rock_1.png", "terrain/rock/rock_2.png", "terrain/rock/rock_3.png"}
 })
 
 DefineTerrainType("fairlimbed-tree", {
 	Name = "Fairlimbed Tree",
 	DefaultBaseTerrain = "grass",
-	Overlay = true
+	Overlay = true,
+	Flags = {"land", "forest", "unpassable"}
 })
 
 DefineTerrainType("flamefood-tree", {
 	Name = "Flamefood Tree",
 	DefaultBaseTerrain = "dry-mud",
-	Overlay = true
+	Overlay = true,
+	Flags = {"land", "forest", "unpassable"}
 })
 
 DefineTerrainType("pine-tree", {
 	Name = "Pine Tree",
 	DefaultBaseTerrain = "grass",
 	Overlay = true,
+	Flags = {"land", "forest", "unpassable"},
 	SolidGraphics = {"terrain/pine_tree/pine_tree.png"},
 	TransitionGraphics = {
 		"any", "north", "terrain/pine_tree/pine_tree_north.png",
