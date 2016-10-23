df4b0615103c93c31420658f4057ee1bb728d459
Andrettin <andre.ng@live.com>
2016-10-23 18:11:09 +0200

    Minor update to Italian bronze age campaign data

--- scripts/map_templates/earth/italy.lua
+++ scripts/map_templates/earth/italy.lua
@@ -25,6 +25,17 @@
 --      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
 
+-- Broglio di Trebisacce
+SetMapTemplateUnit("earth", "unit-latin-farm", "mycenae", {4214, 1143}, -1350, -1150) -- the site of Broglio di Trebisacce (in the plain of Sybaris) was occupied during the Italian Recent Bronze Age (c. 1350-1150 BC) and in the Italian Final Bronze Age (c. 1150-950 BC), and was possibly a Mycenaean colony; the settlement produced Aegean-type storage jars and grew olives in the Recent Bronze Age; in the Final Bronze Age it had some early ironworking, and possessed a wall as well as a ditch; Source: "Ancient Europe 8000 B.C.-A.D. 1000: Encyclopedia of the Barbarian World", 2004, vol. 2, pp. 37-38, 40-41.
+SetMapTemplateUnit("earth", "unit-latin-smithy", "mycenae", {4214, 1143}, -1150, -950)
+
+-- Frattesina
+SetMapTemplateUnit("earth", "unit-germanic-smithy", "celtic-cimmerian-tribe", {4105, 1023}, -1200, -801) -- the site of Frattesina was occupied between the 12th and 9th centuries BC; the settlement imported raw materials (such as amber, ivory, ostrich eggs and copper) and exported manufactured products (such as works in glass, glazed pottery, bone, antler, elephant ivory, bronze, iron and amber); Source: "Ancient Europe 8000 B.C.-A.D. 1000: Encyclopedia of the Barbarian World", 2004, vol. 2, p. 39.
+
+-- Ravenna
+SetMapTemplateUnit("earth", "unit-teuton-town-hall", "ostrogothia", {4114, 1035}, 526) -- Ravenna was a part of the Kingdom of the Ostrogoths in 526; Source: William R. Shepherd, "Historical Atlas", 1911, p. 52.
+
+-- Rome
 SetMapTemplateUnit("earth", "unit-latin-town-hall", "latin-tribe", {4126, 1096}, -750, -500) -- Latins present in Latium by 750-625 BC; Source: William R. Shepherd, "Historical Atlas", 1911, p. 5.
 SetMapTemplateUnit("earth", "unit-latin-town-hall", "rome", {4126, 1096}, -500, 526) -- Rome had been founded and was independent by 500 BC; Source: William R. Shepherd, "Historical Atlas", 1911, p. 29.
 SetMapTemplateUnit("earth", "unit-teuton-temple", "rome", {4126, 1096}, -40, 526) -- Temple of Jupiter Capitolinus, Temple of Jupiter Victor, Temple of Vesta,  Temple of Fortunus and Temple of Aesculapius existed in Rome in c. 40 BC; Source: William R. Shepherd, "Historical Atlas", 1911, pp. 22-23.
@@ -32,5 +43,3 @@ SetMapTemplateUnit("earth", "unit-teuton-dock", "rome", {4126, 1096}, -40, 526) 
 SetMapTemplateUnit("earth", "unit-teuton-town-hall", "ostrogothia", {4126, 1096}, 526) -- Rome was a part of the Kingdom of the Ostrogoths in 526; Source: William R. Shepherd, "Historical Atlas", 1911, p. 52.
 SetMapTemplateUnit("earth", "unit-teuton-temple", "ostrogothia", {4126, 1096}, 526)
 SetMapTemplateUnit("earth", "unit-teuton-dock", "ostrogothia", {4126, 1096}, 526)
-
-SetMapTemplateUnit("earth", "unit-teuton-town-hall", "ostrogothia", {4114, 1035}, 526) -- Ravenna was a part of the Kingdom of the Ostrogoths in 526; Source: William R. Shepherd, "Historical Atlas", 1911, p. 52.
