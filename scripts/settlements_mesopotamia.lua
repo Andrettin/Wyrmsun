b5f7bef283cba2417939550e82aa5a7b3ddbf847
Andrettin <andre.ng@live.com>
2017-02-13 23:26:11 +0100

    More settlement data

--- scripts/settlements_mesopotamia.lua
+++ scripts/settlements_mesopotamia.lua
@@ -25,6 +25,22 @@
 --      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
 
+DefineSettlement("agad", {
+	Name = "Agad",
+	MapTemplate = "earth",
+	Position = {4841, 1290},
+	CulturalNames = {
+		"babylonian", "Agad"
+	},
+	HistoricalOwners = {
+		-1450, "babylon" -- Agad was a Babylonian possession about 1450 BC; Source: William R. Shepherd, "Historical Atlas", 1911, p. 4.
+	},
+	HistoricalBuildings = {
+		-1450, 0, "unit-latin-farm"
+	},
+	Regions = {"asia"}
+})
+
 DefineSettlement("amida", {
 	Name = "Amida",
 	MapTemplate = "earth",
@@ -73,6 +89,118 @@ DefineSettlement("assur", {
 	Regions = {"asia"}
 })
 
+DefineSettlement("babylon", {
+	Name = "Babylon",
+	MapTemplate = "earth",
+	Position = {4848, 1300},
+	CulturalNames = {
+		"babylonian", "Babylon"
+	},
+	HistoricalOwners = {
+		-1450, "babylon" -- Babylon was a Babylonian possession about 1450 BC; Source: William R. Shepherd, "Historical Atlas", 1911, p. 4.
+	},
+	HistoricalBuildings = {
+		-1450, 0, "unit-latin-town-hall"
+	},
+	Regions = {"asia"}
+})
+
+DefineSettlement("borsippa", {
+	Name = "Borsippa",
+	MapTemplate = "earth",
+	Position = {4842, 1300},
+	CulturalNames = {
+		"babylonian", "Borsippa"
+	},
+	HistoricalOwners = {
+		-1450, "babylon" -- Borsippa was a Babylonian possession about 1450 BC; Source: William R. Shepherd, "Historical Atlas", 1911, p. 4.
+	},
+	HistoricalBuildings = {
+		-1450, 0, "unit-latin-farm"
+	},
+	Regions = {"asia"}
+})
+
+DefineSettlement("dur-kurigalzu", {
+	Name = "Dur-Kurigalzu",
+	MapTemplate = "earth",
+	Position = {4853, 1294},
+	CulturalNames = {
+		"babylonian", "Dur-Kurigalzu"
+	},
+	HistoricalOwners = {
+		-1450, "babylon" -- Dur-Kurigalzu was a Babylonian possession about 1450 BC; Source: William R. Shepherd, "Historical Atlas", 1911, p. 4.
+	},
+	HistoricalBuildings = {
+		-1450, 0, "unit-latin-farm"
+	},
+	Regions = {"asia"}
+})
+
+DefineSettlement("eridu", {
+	Name = "Eridu",
+	MapTemplate = "earth",
+	Position = {4902, 1343},
+	CulturalNames = {
+		"babylonian", "Eridu"
+	},
+	HistoricalOwners = {
+		-1450, "babylon" -- Eridu was a Babylonian possession about 1450 BC; Source: William R. Shepherd, "Historical Atlas", 1911, p. 4.
+	},
+	HistoricalBuildings = {
+		-1450, 0, "unit-latin-farm"
+	},
+	Regions = {"asia"}
+})
+
+DefineSettlement("kutha", {
+	Name = "Kutha",
+	MapTemplate = "earth",
+	Position = {4847, 1297},
+	CulturalNames = {
+		"babylonian", "Kutha"
+	},
+	HistoricalOwners = {
+		-1450, "babylon" -- Kutha was a Babylonian possession about 1450 BC; Source: William R. Shepherd, "Historical Atlas", 1911, p. 4.
+	},
+	HistoricalBuildings = {
+		-1450, 0, "unit-latin-farm"
+	},
+	Regions = {"asia"}
+})
+
+DefineSettlement("lagash", {
+	Name = "Lagash",
+	MapTemplate = "earth",
+	Position = {4891, 1326},
+	CulturalNames = {
+		"babylonian", "Lagash"
+	},
+	HistoricalOwners = {
+		-1450, "babylon" -- Lagash was a Babylonian possession about 1450 BC; Source: William R. Shepherd, "Historical Atlas", 1911, p. 4.
+	},
+	HistoricalBuildings = {
+		-1450, 0, "unit-latin-farm"
+	},
+	Regions = {"asia"}
+})
+
+DefineSettlement("larsam", {
+	Name = "Larsam",
+	MapTemplate = "earth",
+	Position = {4883, 1336},
+	CulturalNames = {
+		"babylonian", "Larsam"
+	},
+	HistoricalOwners = {
+		-1450, "babylon" -- Larsam was a Babylonian possession about 1450 BC; Source: William R. Shepherd, "Historical Atlas", 1911, p. 4.
+	},
+	HistoricalBuildings = {
+		-1450, 0, "unit-latin-farm"
+	},
+	Regions = {"asia"}
+})
+
 DefineSettlement("nineveh", {
 	Name = "Nineveh",
 	MapTemplate = "earth",
@@ -88,3 +216,116 @@ DefineSettlement("nineveh", {
 	},
 	Regions = {"asia"}
 })
+
+DefineSettlement("nippur", {
+	Name = "Nippur",
+	MapTemplate = "earth",
+	Position = {4865, 1311},
+	CulturalNames = {
+		"babylonian", "Nippur"
+	},
+	HistoricalOwners = {
+		-1450, "babylon" -- Nippur was a Babylonian possession about 1450 BC; Source: William R. Shepherd, "Historical Atlas", 1911, p. 4.
+	},
+	HistoricalBuildings = {
+		-1450, 0, "unit-latin-farm"
+	},
+	Regions = {"asia"}
+})
+
+DefineSettlement("shinpurla", {
+	Name = "Shinpurla",
+	MapTemplate = "earth",
+	Position = {4902, 1334},
+	CulturalNames = {
+		"babylonian", "Shinpurla"
+	},
+	HistoricalOwners = {
+		-1450, "babylon" -- Shinpurla was a Babylonian possession about 1450 BC; Source: William R. Shepherd, "Historical Atlas", 1911, p. 4.
+	},
+	HistoricalBuildings = {
+		-1450, 0, "unit-latin-farm"
+	},
+	Regions = {"asia"}
+})
+
+DefineSettlement("sippar", {
+	Name = "Sippar",
+	MapTemplate = "earth",
+	Position = {4835, 1286},
+	CulturalNames = {
+		"babylonian", "Sippar"
+	},
+	HistoricalOwners = {
+		-1450, "babylon" -- Sippar was a Babylonian possession about 1450 BC; Source: William R. Shepherd, "Historical Atlas", 1911, p. 4.
+	},
+	HistoricalBuildings = {
+		-1450, 0, "unit-latin-farm"
+	},
+	Regions = {"asia"}
+})
+
+DefineSettlement("susa", {
+	Name = "Susa",
+	MapTemplate = "earth",
+	Position = {4935, 1313},
+	CulturalNames = {
+		"babylonian", "Susa"
+	},
+	HistoricalOwners = {
+		-1450, "babylon" -- Susa was a Babylonian possession about 1450 BC; Source: William R. Shepherd, "Historical Atlas", 1911, p. 4.
+	},
+	HistoricalBuildings = {
+		-1450, 0, "unit-latin-farm"
+	},
+	Regions = {"asia"}
+})
+
+DefineSettlement("upi", {
+	Name = "Upi",
+	MapTemplate = "earth",
+	Position = {4848, 1271},
+	CulturalNames = {
+		"babylonian", "Upi"
+	},
+	HistoricalOwners = {
+		-1450, "babylon" -- Upi was a Babylonian possession about 1450 BC; Source: William R. Shepherd, "Historical Atlas", 1911, p. 4.
+	},
+	HistoricalBuildings = {
+		-1450, 0, "unit-latin-farm"
+	},
+	Regions = {"asia"}
+})
+
+DefineSettlement("ur", {
+	Name = "Ur",
+	MapTemplate = "earth",
+	Position = {4895, 1345},
+	CulturalNames = {
+		"babylonian", "Ur"
+	},
+	HistoricalOwners = {
+		-1450, "babylon" -- Ur was a Babylonian possession about 1450 BC; Source: William R. Shepherd, "Historical Atlas", 1911, p. 4.
+	},
+	HistoricalBuildings = {
+		-1450, 0, "unit-latin-farm"
+	},
+	Regions = {"asia"}
+})
+
+DefineSettlement("uruk", {
+	Name = "Uruk",
+	MapTemplate = "earth",
+	Position = {4879, 1333},
+	CulturalNames = {
+		"babylonian", "Uruk"
+	},
+	HistoricalOwners = {
+		-1450, "babylon" -- Uruk was a Babylonian possession about 1450 BC; Source: William R. Shepherd, "Historical Atlas", 1911, p. 4.
+	},
+	HistoricalBuildings = {
+		-1450, 0, "unit-latin-farm"
+	},
+	Regions = {"asia"}
+})
+
