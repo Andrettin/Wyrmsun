a8d5d19402afd562472196b3b20bd4fc249810ae
Andrettin <andre.ng@live.com>
2018-11-07 15:08:40 +0100

    Added troll warrior icon

--- scripts/civilizations/troll/icons.lua
+++ scripts/civilizations/troll/icons.lua
@@ -24,3 +24,9 @@
 --      along with this program; if not, write to the Free Software
 --      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
+
+DefineIcon({
+	Name = "icon-troll-warrior",
+	Size = {46, 38},
+	File = "troll/icons/warrior.png"
+})
