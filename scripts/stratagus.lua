a400066283305d5ab514ece6b83a1839933069cd
Andrettin <andre.ng@live.com>
2019-01-07 21:39:20 +0100

    Version update

--- scripts/stratagus.lua
+++ scripts/stratagus.lua
@@ -35,7 +35,7 @@ DebugPrint("Stratagus default config file loading ...\n")
 wyrmsun = {}
 
 wyrmsun.Name = "Wyrmsun"
-wyrmsun.Version = "3.5.2"
+wyrmsun.Version = "3.5.3"
 wyrmsun.Homepage = ""
 wyrmsun.Licence = "GPL 2.0"
 wyrmsun.Copyright = "Copyright (c) 2013-2019 by Andrettin"
