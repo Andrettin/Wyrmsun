0d57f051ad3d702a8b106204587ac8e946306cbc
Andrettin <andre.ng@live.com>
2019-01-20 21:37:35 +0100

    Start the Wyrmgus module in its own thread

--- Wyrmsun.gd
+++ Wyrmsun.gd
@@ -1,6 +1,12 @@
 extends Node
 
+var wyrmgus_thread = Thread.new()
+var wyrmgus = WyrmgusModule.new()
+
 func _ready():
-	var wyrmgus = WyrmgusModule.new()
-	wyrmgus.run()
+	wyrmgus_thread.start(self, "run_wyrmgus")
 	pass
+
+func run_wyrmgus(userdata):
+	wyrmgus.run()
+	
