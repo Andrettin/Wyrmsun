e3894f814dacc2c03e72f6fe46222dcbd7be54b7
Andrettin <andre.ng@live.com>
2019-01-25 21:20:54 +0100

    Properly centralize the main menu in the Godot project

--- menu.gd
+++ menu.gd
@@ -4,9 +4,9 @@ var wyrmgus_thread = Thread.new()
 var wyrmgus = WyrmgusModule.new()
 
 func _ready():
+	self.rect_position.x = (OS.window_size.x - 640) / 2
+	self.rect_position.y = (OS.window_size.y - 480) / 2
 	wyrmgus_thread.start(self, "run_wyrmgus")
-	pass
 
 func run_wyrmgus(userdata):
 	wyrmgus.run()
-	
