154aafe3d81c0320b0c3acef97415594f009bc29
Andrettin <andre.ng@live.com>
2019-01-26 09:48:51 +0100

    Button text is now a rich text label

--- button.gd
+++ button.gd
@@ -3,4 +3,9 @@ extends TextureButton
 export var button_text = ""
 
 func _ready():
-	self.find_node("button_text").text = self.button_text
+	var text = self.button_text
+	var text_shadow = self.button_text
+	text_shadow = text_shadow.replace("[color=#f4e020]", "")
+	text_shadow = text_shadow.replace("[/color]", "")
+	self.find_node("button_text").bbcode_text = "[center]" + text + "[/center]"
+	self.find_node("button_text_shadow").bbcode_text = "[center]" + text_shadow + "[/center]"
