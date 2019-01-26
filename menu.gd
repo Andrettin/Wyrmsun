a4f14542bb2e72427e01690e35e849d6f5f9f536
Andrettin <andre.ng@live.com>
2019-01-26 18:28:11 +0100

    Place the menu control in the correct position after the window is resized

--- menu.gd
+++ menu.gd
@@ -4,9 +4,13 @@ var wyrmgus_thread = Thread.new()
 var wyrmgus = WyrmgusModule.new()
 
 func _ready():
-	self.rect_position.x = (OS.window_size.x - 640) / 2
-	self.rect_position.y = (OS.window_size.y - 480) / 2
+	update_position()
+	get_tree().get_root().connect("size_changed", self, "update_position")
 	wyrmgus_thread.start(self, "run_wyrmgus")
 
 func run_wyrmgus(userdata):
 	wyrmgus.run()
+
+func update_position():
+	self.rect_position.x = (OS.window_size.x - 640) / 2
+	self.rect_position.y = (OS.window_size.y - 480) / 2
