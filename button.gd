06c4fc45c1f8d536486429160e5afc218a58bfec
Andrettin <andre.ng@live.com>
2019-01-26 11:08:40 +0100

    Button text is now drawn properly when the button is hovered

--- button.gd
+++ button.gd
@@ -4,9 +4,11 @@ export var button_text = ""
 const character_highlight_markdown = "~!"
 const highlight_color = "#f4e020"
 var display_pressed_text = false
+var text = ""
+var hovered_text = ""
 
 func _ready():
-	var text = self.button_text
+	text = self.button_text
 	var find_pos = text.find(character_highlight_markdown)
 	while (find_pos != -1):
 		text = text.insert(find_pos + character_highlight_markdown.length() + 1, "[/color]")
@@ -17,17 +19,31 @@ func _ready():
 	var text_shadow = text
 	text_shadow = text_shadow.replace("[color=" + highlight_color + "]", "")
 	text_shadow = text_shadow.replace("[/color]", "")
-	self.find_node("button_text").bbcode_text = "[center]" + text + "[/center]"
-	self.find_node("button_text_shadow").bbcode_text = "[center]" + text_shadow + "[/center]"
+	hovered_text = "[center][color=" + highlight_color + "]" + text_shadow + "[/color][/center]"
+	text = "[center]" + text + "[/center]"
+	text_shadow = "[center]" + text_shadow + "[/center]"
+	self.find_node("button_text").bbcode_text = text
+	self.find_node("button_text_shadow").bbcode_text = text_shadow
 	
-	self.connect("draw", self, "check_set_display_pressed_text")
-
-func check_set_display_pressed_text():
+	self.connect("draw", self, "check_set_text_display_pressed")
+	
+	self.connect("mouse_entered", self, "display_text_hovered")
+	self.connect("mouse_exited", self, "display_text_not_hovered")
+	
+func check_set_text_display_pressed():
 	if (display_pressed_text == false and self.get_draw_mode() == DRAW_PRESSED):
 		display_pressed_text = true
 		self.find_node("button_text").rect_position += Vector2(2, 2)
 		self.find_node("button_text_shadow").rect_position += Vector2(2, 2)
+		self.find_node("button_text").bbcode_text = hovered_text
 	elif (display_pressed_text == true and self.get_draw_mode() != DRAW_PRESSED):
 		display_pressed_text = false
 		self.find_node("button_text").rect_position -= Vector2(2, 2)
 		self.find_node("button_text_shadow").rect_position -= Vector2(2, 2)
+		self.find_node("button_text").bbcode_text = text #this is necessary because the mouse counts as not having exited while the button is pressed, regardless of mouse position
+
+func display_text_hovered():
+	self.find_node("button_text").bbcode_text = hovered_text
+
+func display_text_not_hovered():
+	self.find_node("button_text").bbcode_text = text
