2fdb9e6359c68ebaed45ca3b9e4faac3138b8d65
Andrettin <andre.ng@live.com>
2019-01-27 20:17:51 +0100

    Update to main menu scene

--- scripts/guichan.lua
+++ scripts/guichan.lua
@@ -32,7 +32,7 @@ dark = Color(38, 38, 78)
 clear = Color(200, 200, 120)
 black = Color(0, 0, 0)
 
-bckground = CGraphic:New("ui/Menu_background_with_title.png")
+bckground = CGraphic:New("backgrounds/cosmos.png")
 bckground:Load()
 bckground:Resize(Video.Width, Video.Height)
 backgroundWidget = ImageWidget(bckground)
--- scripts/logo.gd
+++ scripts/logo.gd
@@ -1,12 +1,8 @@
 extends TextureRect
 
 func _ready():
-	update_size()
-	get_tree().get_root().connect("size_changed", self, "update_size")
+	update_position()
+	get_tree().get_root().connect("size_changed", self, "update_position")
 
-func update_size():
-	self.rect_size = Vector2(OS.window_size.x / 2, OS.window_size.y / 4)
-	self.texture = preload("res://graphics/interface/wyrmsun_logo_1_8th.png")
-	if (self.texture.get_width() > self.rect_size.x or self.texture.get_height() > self.rect_size.y):
-		self.texture = preload("res://graphics/interface/wyrmsun_logo_1_16th.png")
-	self.rect_position.x = (OS.window_size.x / 2) - (self.rect_size.x / 2)
+func update_position():
+	self.rect_position.x = OS.window_size.x / 2 - self.rect_size.x / 2
