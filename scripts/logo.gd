66a9da58ab6b996872913dcfe4ced3874473d522
Andrettin <andre.ng@live.com>
2019-02-13 07:17:19 +0100

    Use anchors instead of the logo.gd script to place the logo correctly in its position

--- scripts/logo.gd
+++
@@ -1,8 +0,0 @@
-extends TextureRect
-
-func _ready():
-	update_position()
-	get_tree().get_root().connect("size_changed", self, "update_position")
-
-func update_position():
-	self.rect_position.x = OS.window_size.x / 2 - self.rect_size.x / 2
