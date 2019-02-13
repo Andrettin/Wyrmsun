af33d48fe04f0d9decb02fdf169f0c7ddb874054
Andrettin <andre.ng@live.com>
2019-02-13 07:09:22 +0100

    Use anchors instead of the screen.gd script to resize windows

--- scripts/start_scenario_button.gd
+++ scripts/start_scenario_button.gd
@@ -10,3 +10,7 @@ func _pressed():
 	command += "\nRunMap(\"scripts/map_templates/campaign.smp\")"
 	
 	wyrmgus.lua_command(command)
+	
+	get_tree().change_scene("res://scenes/scenario.tscn")
+	#changing the scene will remove this button's audio player (resulting in the sound not being played), so we need to make the global audio player play it
+	audio_player.play_sound_file("res://sounds/interface/click.wav")
