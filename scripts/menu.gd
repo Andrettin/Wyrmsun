7139a092f8e81754aebcfded56648eab10a058dd
Andrettin <andre.ng@live.com>
2019-02-13 07:13:38 +0100

    Use anchors instead of the menu.gd script for placing the menu area in the correct position

--- scripts/menu.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-func _ready():
-	update_position()
-	get_tree().get_root().connect("size_changed", self, "update_position")
-
-func update_position():
-	self.rect_position.x = (OS.window_size.x - self.rect_size.x) / 2
-	self.rect_position.y = (OS.window_size.y - self.rect_size.y) / 2
