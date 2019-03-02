8dd8babf2e4ff0a791cdaba7aa64ca096317e72f
Andrettin <andre.ng@live.com>
2019-03-02 09:48:41 +0100

    Added achievement buttons to the achievements menu scene

--- scripts/achievements_menu.gd
+++ scripts/achievements_menu.gd
@@ -15,10 +15,17 @@ func _ready():
 		var achievement_button = load("res://scenes/icon_button.tscn").instance()
 		menu_area_node.add_child(achievement_button)
 		
-		achievement_button.set_graphics("res://graphics/" + achievement.get_icon().get_file())
+		var graphics_file = achievement.get_icon().get_file()
+		if (graphics_file.find("dlcs/") != -1):
+			graphics_file = "res://" + graphics_file
+		else:
+			graphics_file = "res://graphics/" + graphics_file
+		
+		achievement_button.set_graphics(graphics_file)
+		achievement_button.set_player_color(achievement.get_player_color())
 		
 		achievement_button.rect_position.x = 23 + 4 + (54 * item_x)
-		achievement_button.rect_position.y = 10 + 4 + (46 * item_y)
+		achievement_button.rect_position.y = 10 + 4 + (36 * 1.5) + (46 * item_y)
 		
 		item_x = item_x + 1
 		
