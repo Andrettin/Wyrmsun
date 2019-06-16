f7cd35a409b237f0ec9068c9a8774b26c8ef0333
Andrettin <andre.ng@live.com>
2019-06-16 12:51:30 +0200

    Set the source color for a unit sprite based on that of the image

--- scripts/unit.gd
+++ scripts/unit.gd
@@ -19,6 +19,10 @@ func set_image(new_image):
 	self.texture = new_image.get_texture()
 	self.hframes = new_image.get_texture().get_width() / new_image.get_frame_size().x
 	self.vframes = new_image.get_texture().get_height() / new_image.get_frame_size().y
+	
+	if (new_image.get_source_primary_player_color() != null):
+		self.source_primary_player_color = new_image.get_source_primary_player_color()
+	self.source_secondary_player_color = new_image.get_source_secondary_player_color()
 
 func set_tile_pos(tile_pos, half_tile_pixel_size):
 	self.position = (tile_pos * wyrmgus.get_pixel_tile_size()) + half_tile_pixel_size
