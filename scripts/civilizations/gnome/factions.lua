9f82bfe93617f5e20d5dc75009ced77e9992179f
Andrettin <andre.ng@live.com>
2019-06-26 17:24:20 +0200

    Made Gnomish factions use the new dwarven color-conversible flag

--- scripts/civilizations/gnome/factions.lua
+++ scripts/civilizations/gnome/factions.lua
@@ -30,8 +30,9 @@ DefineFaction("acthnic-tribe", { -- Source: Franz Hartmann, "Among the Gnomes", 
 	Civilization = "gnome",
 	Type = "tribe",
 	PrimaryColor = "red",
+	SecondaryColor = "black",
 	FactionUpgrade = "upgrade-faction-acthnic-tribe",
-	Icon = "icon-flag-silver-hammer-on-red",
+	Icon = "icon-flag-white-hammer-on-red",
 	Description = "The Acthnic tribe lives deep underground, near the subterranean fires of their homelands. They're known for being dangerous and reckless, occasionally entering the territory of derros or goblins without invitation. Their sages are well-versed in the mysteries of lightning, but above all they are masters of the flame."
 })
 
@@ -40,7 +41,8 @@ DefineFaction("cubital-tribe", { -- Source: Franz Hartmann, "Among the Gnomes", 
 	Civilization = "gnome",
 	Type = "tribe",
 	PrimaryColor = "brown",
-	Icon = "icon-flag-light-blue-hammer-on-orange",
+	SecondaryColor = "red",
+	Icon = "icon-flag-white-hammer-on-red",
 	Description = "The Cubitals are a hard-working tribe of gnomes, being known for their muscular strength and their efficiency at stonecutting. They are also talented artistically, making Cubital products highly sought after by the gnomish upper classes. When engaging in warfare, the Cubitals are known to make heavy use of explosives. The tribe's priests strive to command the forces of the earth and the air."
 })
 
@@ -52,7 +54,7 @@ DefineFaction("derro-tribe", {
 	SecondaryColor = "purple",
 	Playable = false,
 	FactionUpgrade = "upgrade-faction-derro-tribe",
-	Icon = "icon-flag-yellow-hammer-on-purple",
+	Icon = "icon-flag-white-hammer-on-red",
 	Description = "The Derro are a cruel gnomish offshoot, which separated themselves from the rest of their kin aeons ago. They dwell in the dark depths of Nidavellir's tunnels, in caves isolated even from other underground denizens. These gnomish fiends think themselves uniquely gifted, being at the same time wary and disdainful of other peoples. They see individuals of other races trespassing on their territory as a great crime, and will gladly put them in their place with the most sadistic of measures."
 })
 
@@ -61,8 +63,9 @@ DefineFaction("sagan-tribe", { -- Source: Franz Hartmann, "Among the Gnomes", 18
 	Civilization = "gnome",
 	Type = "tribe",
 	PrimaryColor = "green",
+	SecondaryColor = "brown",
 	FactionUpgrade = "upgrade-faction-sagan-tribe",
-	Icon = "icon-flag-cyan-hammer-on-green",
+	Icon = "icon-flag-white-hammer-on-red",
 	Description = "Herbalists par excellence, the Sagans are devoted to tending to the body and soul of plants. The tribesfolk is known for being tall (for gnomes)."
 })
 
@@ -71,7 +74,8 @@ DefineFaction("vulcan-tribe", { -- Source: Franz Hartmann, "Among the Gnomes", 1
 	Civilization = "gnome",
 	Type = "tribe",
 	PrimaryColor = "yellow",
-	Icon = "icon-flag-blue-hammer-on-yellow",
+	SecondaryColor = "red",
+	Icon = "icon-flag-white-hammer-on-red",
 	Description = "The Vulcans excel at mining and metallurgy. The tribe's sages are believed to possess power over fire and thunder."
 })
 
@@ -81,8 +85,9 @@ DefineFaction("acthna", {
 	Civilization = "gnome",
 	Type = "polity",
 	PrimaryColor = "red",
+	SecondaryColor = "black",
 	DefaultTier = "duchy",
-	Icon = "icon-flag-silver-hammer-on-red",
+	Icon = "icon-flag-white-hammer-on-red",
 	HistoricalCapitals = {
 		-3000, "acthna"
 	}
@@ -93,8 +98,9 @@ DefineFaction("untersberg", { -- Source: Franz Hartmann, "Among the Gnomes", 189
 	Civilization = "gnome",
 	Type = "polity",
 	PrimaryColor = "blue", -- the king of Untersberg, Pypo, wore a steel-blue gown; Source: Franz Hartmann, "Among the Gnomes", 1895, pp. 12-13.
+	SecondaryColor = "white",
 	DefaultTier = "kingdom",
-	Icon = "icon-flag-green-hammer-on-blue",
+	Icon = "icon-flag-white-hammer-on-red",
 	MinisterTitles = {
 		"head-of-government", "", "no-government-type", "no-faction-tier", "Grand Chancellor", -- Source: Franz Hartmann, "Among the Gnomes", 1895, pp. 201, 206.
 		"education-minister", "", "no-government-type", "no-faction-tier", "Superintendent of Public Schools" -- Source: Franz Hartmann, "Among the Gnomes", 1895, p. 201.
