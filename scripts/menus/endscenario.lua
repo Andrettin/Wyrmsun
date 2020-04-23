f63c28e5313712711ddad2d2a7d83bcbcad67549
Andrettin <6322423+Andrettin@users.noreply.github.com>
2020-04-23 08:47:35 +0200

    Updated a few more menus for the scale factor

--- scripts/menus/endscenario.lua
+++ scripts/menus/endscenario.lua
@@ -1,8 +1,8 @@
 function RunEndScenarioMenu()
 	local menu = WarGameMenu(panel(1))
 
-	menu:addLabel(_("End Mission"), 128, 11)
-	local b = menu:addFullButton(_("~!Restart Mission"), "r", 16, 40 + (36 * 0),
+	menu:addLabel(_("End Mission"), 128 * get_scale_factor(), 11 * get_scale_factor())
+	local b = menu:addFullButton(_("~!Restart Mission"), "r", 16 * get_scale_factor(), (40 + (36 * 0)) * get_scale_factor(),
 		function()
 			if not (IsNetworkGame()) then
 				RunRestartConfirmMenu()
@@ -14,13 +14,13 @@ function RunEndScenarioMenu()
 	end
 --	menu:addFullButton("~!Resign", "r", 16, 40 + (36 * 1),
 --		function() RunSurrenderConfirmMenu() end)
-	b = menu:addFullButton(_("~!Quit Mission"), "q", 16, 40 + (36 * 1),
+	b = menu:addFullButton(_("~!Quit Mission"), "q", 16 * get_scale_factor(), (40 + (36 * 1)) * get_scale_factor(),
 		function() RunQuitToMenuConfirmMenu() end)
 	
-	menu:addFullButton(_("E~!xit to Desktop"), "x", 16, 40 + (36 * 2),
+	menu:addFullButton(_("E~!xit to Desktop"), "x", 16 * get_scale_factor(), (40 + (36 * 2)) * get_scale_factor(),
 		function() RunExitConfirmMenu() end
 	)
-	menu:addFullButton(_("Previous Menu (~<Esc~>)"), "escape", 16, 248,
+	menu:addFullButton(_("Previous Menu (~<Esc~>)"), "escape", 16 * get_scale_factor(), 248 * get_scale_factor(),
 		function() menu:stop() end
 	)
 
@@ -30,10 +30,10 @@ end
 function RunRestartConfirmMenu()
   local menu = WarGameMenu(panel(1))
 
-  menu:addLabel(_("Are you sure?"), 128, 11)
-  menu:addFullButton(_("~!Yes"), "y", 16, 11 + (36 * 2) + 29,
+  menu:addLabel(_("Are you sure?"), 128 * get_scale_factor(), 11 * get_scale_factor())
+  menu:addFullButton(_("~!Yes"), "y", 16 * get_scale_factor(), (11 + (36 * 2) + 29) * get_scale_factor(),
     function() StopGame(GameRestart); menu:stopAll() end)
-  menu:addFullButton(_("~!No"), "n", 16, 11 + (36 * 3) + 29,
+  menu:addFullButton(_("~!No"), "n", 16 * get_scale_factor(), (11 + (36 * 3) + 29) * get_scale_factor(),
     function() menu:stop() end)
 
   menu:run(false)
@@ -42,10 +42,10 @@ end
 function RunSurrenderConfirmMenu()
   local menu = WarGameMenu(panel(1))
 
-  menu:addLabel(_("Are you sure?"), 128, 11)
-  menu:addFullButton(_("~!Yes"), "y", 16, 11 + (36 * 2) + 29,
+  menu:addLabel(_("Are you sure?"), 128 * get_scale_factor(), 11 * get_scale_factor())
+  menu:addFullButton(_("~!Yes"), "y", 16 * get_scale_factor(), (11 + (36 * 2) + 29) * get_scale_factor(),
     function() StopGame(GameDefeat); menu:stopAll() end)
-  menu:addFullButton(_("~!No"), "n", 16, 11 + (36 * 3) + 29,
+  menu:addFullButton(_("~!No"), "n", 16 * get_scale_factor(), (11 + (36 * 3) + 29) * get_scale_factor(),
     function() menu:stop() end)
 
   menu:run(false)
@@ -54,10 +54,10 @@ end
 function RunQuitToMenuConfirmMenu()
 	local menu = WarGameMenu(panel(1))
 
-	menu:addLabel(_("Are you sure?"), 128, 11)
-	menu:addFullButton(_("~!Yes"), "y", 16, 11 + (36 * 2) + 29,
+	menu:addLabel(_("Are you sure?"), 128 * get_scale_factor(), 11 * get_scale_factor())
+	menu:addFullButton(_("~!Yes"), "y", 16 * get_scale_factor(), (11 + (36 * 2) + 29) * get_scale_factor(),
 		function() StopMusic(); StopGame(GameQuitToMenu); Editor.Running = EditorNotRunning; menu:stopAll() end)
-	menu:addFullButton(_("~!No"), "n", 16, 11 + (36 * 3) + 29,
+	menu:addFullButton(_("~!No"), "n", 16 * get_scale_factor(), (11 + (36 * 3) + 29) * get_scale_factor(),
 		function() menu:stop() end)
 
 	menu:run(false)
@@ -66,12 +66,11 @@ end
 function RunExitConfirmMenu()
   local menu = WarGameMenu(panel(1))
 
-  menu:addLabel(_("Are you sure?"), 128, 11)
-  menu:addFullButton(_("~!Yes"), "y", 16, 11 + (36 * 2) + 29,
+  menu:addLabel(_("Are you sure?"), 128 * get_scale_factor(), 11 * get_scale_factor())
+  menu:addFullButton(_("~!Yes"), "y", 16 * get_scale_factor(), (11 + (36 * 2) + 29) * get_scale_factor(),
     function() Exit(0) end)
-  menu:addFullButton(_("~!No"), "n", 16, 11 + (36 * 3) + 29,
+  menu:addFullButton(_("~!No"), "n", 16 * get_scale_factor(), (11 + (36 * 3) + 29) * get_scale_factor(),
     function() menu:stop() end)
 
   menu:run(false)
 end
-
--- scripts/menus/game.lua
+++ scripts/menus/game.lua
@@ -31,25 +31,25 @@ end
 function RunGameMenu()
 	local menu = WarGameMenu(panel(1))
 
-	menu:addLabel(_("Game Menu"), 128, 11)
+	menu:addLabel(_("Game Menu"), 128 * get_scale_factor(), 11 * get_scale_factor())
 
-	menu:addFullButton(_("Options (~<F5~>)"), "f5", 16, 40,
+	menu:addFullButton(_("Options (~<F5~>)"), "f5", 16 * get_scale_factor(), 40 * get_scale_factor(),
 		function()
 		RunGameOptionsMenu(menu)
 	end)
-	menu:addFullButton(_("Help (~<F1~>)"), "f1", 16, 40 + 34*1,
+	menu:addFullButton(_("Help (~<F1~>)"), "f1", 16 * get_scale_factor(), (40 + 34*1) * get_scale_factor(),
 		function() RunHelpMenu() end)
-	local b = menu:addFullButton(_("Save (~<F11~>)"), "f11", 16, 40 + 34*2,
+	local b = menu:addFullButton(_("Save (~<F11~>)"), "f11", 16 * get_scale_factor(), (40 + 34*2) * get_scale_factor(),
 		function() RunSaveMenu() end
 	)
  
-	b = menu:addFullButton(_("Load (~<F12~>)"), "f12", 16, 40 + 34*3,
+	b = menu:addFullButton(_("Load (~<F12~>)"), "f12", 16 * get_scale_factor(), (40 + 34*3) * get_scale_factor(),
 		function() RunGameLoadGameMenu() end
 	)
 	
-	menu:addFullButton(_("~!End Mission"), "e", 16, 40 + 34*4,
+	menu:addFullButton(_("~!End Mission"), "e", 16 * get_scale_factor(), (40 + 34*4) * get_scale_factor(),
 		function() RunEndScenarioMenu() end)
-	menu:addFullButton(_("Return to Game (~<Esc~>)"), "escape", 16, 40 + 34*5,
+	menu:addFullButton(_("Return to Game (~<Esc~>)"), "escape", 16 * get_scale_factor(), (40 + 34*5) * get_scale_factor(),
 		function() menu:stop() end)
 
 	menu:run(false)
