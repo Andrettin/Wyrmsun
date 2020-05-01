3b2118894746e83b3deb5d34dad0a544248719b4
Andrettin <6322423+Andrettin@users.noreply.github.com>
2020-05-01 11:44:33 +0200

    Added 2x scaled versions of the wood plow, iron-tipped wood plow and iron plow icons

--- scripts/icons.lua
+++ scripts/icons.lua
@@ -356,24 +356,6 @@ DefineIcon({
 })
 
 DefineIcon({
-	Name = "icon-wood-plow",
-	Size = {46, 38},
-	File = "neutral/icons/wood_plow.png"
-})
-
-DefineIcon({
-	Name = "icon-iron-tipped-wood-plow",
-	Size = {46, 38},
-	File = "neutral/icons/iron_tipped_wood_plow.png"
-})
-
-DefineIcon({
-	Name = "icon-iron-plow",
-	Size = {46, 38},
-	File = "neutral/icons/iron_plow.png"
-})
-
-DefineIcon({
 	Name = "icon-masonry",
 	Size = {46, 38},
 	File = "neutral/icons/masonry.png"
