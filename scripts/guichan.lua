1e7e1e168e9ef33c5875fd86e05747dd3f7d0c88
Andrettin <6322423+Andrettin@users.noreply.github.com>
2020-09-05 14:47:49 +0200

    Minor guichan.lua update

--- scripts/guichan.lua
+++ scripts/guichan.lua
@@ -32,6 +32,9 @@ dark = Color(38, 38, 78)
 clear = Color(200, 200, 120)
 black = Color(0, 0, 0)
 
+color_bckground = "backgrounds/wyrmsun.png"
+color_background_widget = ImageWidget(color_bckground, 1, Video.Width, Video.Height)
+
 bckground = "backgrounds/wyrmsun_sepia.png"
 backgroundWidget = ImageWidget(bckground, 1, Video.Width, Video.Height)
 
