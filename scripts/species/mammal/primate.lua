33b8fe758f83ead08177ff17eccce5d488ba60d8
Andrettin <6322423+Andrettin@users.noreply.github.com>
2020-09-06 09:20:52 +0200

    Moved more sapient primate species definitions (e.g. ettins, ogres, goblins) to the new database format

--- scripts/species/mammal/primate.lua
+++ scripts/species/mammal/primate.lua
@@ -25,40 +25,11 @@
 --      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
 
-DefineSpeciesGenus("gigans", {
-	Name = "Gigans", -- "giant" in Latin
-	CommonName = "Giant"
-})
-
-DefineSpeciesGenus("gobelinus", {
-	Name = "Gobelinus", -- "goblin" in Latin
-	CommonName = "Goblinoid"
-})
-
-DefineSpeciesGenus("orcus", {
-	Name = "Orcus", -- "orc" in Latin
-	CommonName = "Orcoid"
-})
-
 DefineSpecies("plesiadapis", {
 	Name = "Plesiadapis",
 	Supertaxon = "primates",
 	Homeworld = "earth",
 	EvolvesFrom = {"alphadon", "leptictidium", "zalambdalestes"}
-})
-
-DefineSpecies("lemur", {
-	Name = "Lemur",
-	Supertaxon = "primates",
-	Homeworld = "earth",
-	EvolvesFrom = {"plesiadapis"}
-})
-
-DefineSpecies("monkey", {
-	Name = "Monkey",
-	Supertaxon = "primates",
-	Homeworld = "earth",
-	EvolvesFrom = {"plesiadapis"}
 })
 
 DefineSpecies("dryopithecus", {
@@ -68,79 +39,10 @@ DefineSpecies("dryopithecus", {
 	EvolvesFrom = {"plesiadapis"}
 })
 
-DefineSpecies("chimpanzee", {
-	Name = "Chimpanzee",
-	Supertaxon = "primates",
-	Homeworld = "earth",
-	EvolvesFrom = {"dryopithecus"}
-})
-
 DefineSpecies("australopithecus", {
 	Name = "Australopithecus",
 	Genus = "australopithecus",
 	Species = "afarensis", -- the Australopithecus afarensis is also known as Praeanthropus africanus; Source: http://fossilworks.org/bridge.pl?a=taxonInfo&taxon_no=83054
 	Homeworld = "earth",
 	EvolvesFrom = {"dryopithecus"}
-})
-
-DefineSpecies("cyclops", {
-	Name = "Cyclops",
-	Genus = "gigans", -- "giant" in Latin; cyclops are of the same genus as ettins
-	Species = "cyclops",
-	Homeworld = "jotunheim",
-	Era = "holocene",
-	Sapient = true
-})
-
-DefineSpecies("ettin", {
-	Name = "Ettin",
-	Genus = "gigans", -- "giant" in Latin
-	Species = "sapiens",
-	Homeworld = "jotunheim",
-	Era = "holocene",
-	Sapient = true
-})
-
-DefineSpecies("goblin", {
-	Name = "Goblin",
-	Genus = "gobelinus", -- "goblin" in Latin
-	Species = "brevis", -- "small"/"short" in Latin (as it is smaller than a hobgoblin)
-	Homeworld = "nidavellir",
-	Era = "holocene",
-	Sapient = true
-})
-
-DefineSpecies("hobgoblin", {
-	Name = "Hobgoblin",
-	Genus = "gobelinus", -- "goblin" in Latin
-	Species = "magnus", -- "big" in Latin" (as it is bigger than a goblin)
-	Homeworld = "nidavellir",
-	Era = "holocene",
-	Sapient = true
-})
-
-DefineSpecies("ogre", {
-	Name = "Ogre",
-	Supertaxon = "primates",
-	Species = "sapiens",
-	Homeworld = "jotunheim",
-	Era = "holocene",
-	Sapient = true
-})
-
-DefineSpecies("orc", {
-	Name = "Orc",
-	Genus = "orcus", -- "orc" in Latin
-	Species = "sapiens",
-	Homeworld = "jotunheim",
-	Era = "holocene",
-	Sapient = true
-})
-
-DefineSpecies("troll", {
-	Name = "Troll",
-	Supertaxon = "primates",
-	Homeworld = "alfheim",
-	Era = "holocene",
-	Sapient = true
 })
