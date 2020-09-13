c254ffc8d06ff06904e6e26f0c0e6c785c07a82c
Andrettin <6322423+Andrettin@users.noreply.github.com>
2020-09-13 18:15:23 +0200

    Moved the definitions of the Far Sight and Healing spells from Lua to the new database format

--- scripts/spells.lua
+++ scripts/spells.lua
@@ -32,40 +32,6 @@
 DefineUnitType("unit-revealer", {})
 DefineUnitType("unit-ethereal-revealer", {})
 
-DefineSpell("spell-far-sight",
-	"showname", "Far Sight",
-	"manacost", 70,
-	"range", "infinite",
-	"target", "position",
-	"stackable", false,
-	"action", {
-		{"summon", "unit-type", "unit-ethereal-revealer", "time-to-live", 50},
-		{"spawn-missile", "missile", "missile-magic-effect", "start-point", {"base", "target"}}
-	},
-	"sound-when-cast", "magic-holy",
-	"depend-upgrade", "upgrade-far-sight"
-)
-
-DefineSpell("spell-healing",
-	"showname", "Healing",
-	"manacost", 2,
-	"range", 6,
-	"target", "unit",
-	"action", {
-		{"adjust-vitals", "hit-points", 1, "max-multi-cast", 4},
-		{"spawn-missile", "missile", "missile-magic-effect", "start-point", {"base", "target"}}
-	},
-	"condition", {
-		"Organic", "only",
-		"Building", "false",
-		"HitPoints", {MaxValuePercent = 100}
-	},
-	"sound-when-cast", "magic-holy",
-	"depend-upgrade", "upgrade-healing",
-	"autocast", {"range", 6, "condition", {"alliance", "only", "HitPoints", {MaxValuePercent = 90}}},
-	"ai-cast", {"range", 6, "condition", {"alliance", "only", "HitPoints", {MaxValuePercent = 90}}}
-)
-
 DefineSpell("spell-herbal-cure",
 	"showname", "Herbal Cure",
 	"manacost", 1,
