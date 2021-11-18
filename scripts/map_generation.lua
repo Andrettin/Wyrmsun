842a9902cac08f04bf54640749f460ae286aa031
Andrettin <6322423+Andrettin@users.noreply.github.com>
2021-11-18 19:09:17 +0100

    Adjusted custom maps to have building sites, so that universities can be built

--- scripts/map_generation.lua
+++ scripts/map_generation.lua
@@ -2338,7 +2338,7 @@ function GenerateRandomMap(arg)
 			CreateGoldSpots((CMap:get():get_info().MapWidth * CMap:get():get_info().MapHeight) / 2048, 0, CMap:get():get_info().MapWidth - 3, 0, CMap:get():get_info().MapHeight - 3, symmetric)
 		end
 
-		--CreateNeutralBuildings("unit_building_site", (CMap:get():get_info().MapWidth * CMap:get():get_info().MapHeight) / 1024, 0, CMap:get():get_info().MapWidth - 3, 0, CMap:get():get_info().MapHeight - 3, symmetric)
+		CreateNeutralBuildings("unit_building_site", (CMap:get():get_info().MapWidth * CMap:get():get_info().MapHeight) / 1024, 0, CMap:get():get_info().MapWidth - 3, 0, CMap:get():get_info().MapHeight - 3, symmetric)
 		
 		if (arg.MercenaryCamp) then
 			CreateNeutralBuildings("unit-mercenary-camp", 1, 0, CMap:get():get_info().MapWidth - 3, 0, CMap:get():get_info().MapHeight - 3, symmetric)
