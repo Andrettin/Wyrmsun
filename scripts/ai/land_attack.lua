8d482b33ec19c7ad5cd67ae72e2f0168b8f0b121
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-01-01 18:26:17 +0100

    Updated some copyright headers for 2022

--- scripts/ai/land_attack.lua
+++ scripts/ai/land_attack.lua
@@ -10,7 +10,7 @@
 --
 --      land_attack.lua - Default land attack AI.
 --
---      (c) Copyright 2000-2021 by José Ignacio Rodríguez, Carlo Almario and Andrettin
+--      (c) Copyright 2000-2022 by José Ignacio Rodríguez, Carlo Almario and Andrettin
 --
 --      This program is free software; you can redistribute it and/or modify
 --      it under the terms of the GNU General Public License as published by
