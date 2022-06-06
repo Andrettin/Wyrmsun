b41ed6bfabec8750e6cd74312c12a2b438ba4a16
Andrettin <6322423+Andrettin@users.noreply.github.com>
2022-06-06 12:47:27 +0200

    Moved the RGB change application to its own function in the shader

--- shaders/fragment_shader.glsl
+++ shaders/fragment_shader.glsl
@@ -6,14 +6,20 @@ uniform highp float red_change;
 uniform highp float green_change;
 uniform highp float blue_change;
 
-void main() {
-	highp vec4 tmp_frag_color = texture2D(src, qt_TexCoord0);
 
+void apply_rgb_change(inout highp vec4 tmp_frag_color)
+{
 	tmp_frag_color.r = min(max(tmp_frag_color.r + red_change, 0.0), 1.0);
 	tmp_frag_color.g = min(max(tmp_frag_color.g + green_change, 0.0), 1.0);
 	tmp_frag_color.b = min(max(tmp_frag_color.b + blue_change, 0.0), 1.0);
+}
 
+void main()
+{
+	highp vec4 tmp_frag_color = texture2D(src, qt_TexCoord0);
+
+	apply_rgb_change(tmp_frag_color);
 	tmp_frag_color.a *= opacity_multiplier;
-
+	
 	gl_FragColor = tmp_frag_color * tmp_frag_color.a;
 };
